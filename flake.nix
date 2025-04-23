8b69d2f7c606118708e67c6c0752a103ce13e2e3
guiltedrose <guiltedrose@proton.me>
2025-04-22 22:41:53 -0400

    Updated flake

--- flake.nix
+++ flake.nix
@@ -5,30 +5,22 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, home-manager, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-      in {
-        nixosConfigurations.penguin = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./configuration.nix
-            ./hardware-configuration.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useUserPackages = true;
-              home-manager.useGlobalPkgs = true;
-              home-manager.users.zelda = import ./home.nix;
-            }
-          ];
-        };
-      });
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations.penguin = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        ./hardware-configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useUserPackages = true;
+          home-manager.useGlobalPkgs = true;
+          home-manager.users.zelda = import ./home.nix;
+        }
+      ];
+    };
+  };
 }
 
